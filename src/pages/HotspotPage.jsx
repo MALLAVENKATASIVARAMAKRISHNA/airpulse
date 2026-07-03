2d9d2ab3b42ac2c266dda47682d93838cc0aad73
Ubuntu <ubuntu@ip-172-31-7-196.ap-south-1.compute.internal>
2026-07-03 06:52:14 +0000

    Switch Hotspots map to CartoDB Positron light theme tiles for a clean, light-grey geographic layout

--- src/pages/HotspotPage.jsx
+++ src/pages/HotspotPage.jsx
@@ -45,7 +45,7 @@ export default function HotspotPage({ profile }) {
 
       window.L.control.zoom({ position: 'bottomright' }).addTo(map)
 
-      window.L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+      window.L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
         maxZoom: 20
       }).addTo(map)
